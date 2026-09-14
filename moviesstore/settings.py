e5a28db8ecdeb7c4c9e4e611f45c007b69def5bd
Justin Jordan <justinhiramjordan1@gmail.com>
2026-09-13 22:55:27 -0400

    SQLite revert

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-suy8026r$c9nvc3_97wrv4cm1&$4+2_@*-9u@lw8wi2ae8w-u5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jjord311.pythonanywhere.com', 'localhost', '127.0.0.1']
 import os
 
 
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
